37c82f4948fc9e48087a977bbca10eef713ad586
abdulkarimov <79849423+abdulkarimov@users.noreply.github.com>
2021-10-12 01:55:31 +0600

    ss

---
+++ WpfApp6/WpfApp6/MainWindow.xaml.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp6
+{
+    public partial class MainWindow : Window
+    {
+        string address = "127.0.0.1"; 
+        int port = 8005;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        public void DoWork()
+        {
+            int count = 0;
+            while (true)
+            {
+                WebRequest request = WebRequest.Create(@"https://2452238.com");
+                try
+                {
+                    HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+                    count = 0;
+                }
+                catch (Exception)
+                {
+                    count++;
+                    if (count == 5) // Должно быть 60 но это слишком ДОЛГО
+                    {
+                        
+                        IPEndPoint ipPoint = new IPEndPoint(IPAddress.Parse(address), port);
+                        Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+                        socket.Connect(ipPoint);
+
+                        string message = " 2452238.com не отвечает уже минуту";
+                        byte[] data = Encoding.Unicode.GetBytes(message);
+                        socket.Send(data);
+                    }
+                }
+                Thread.Sleep(1000); // 1 сек
+            }
+        }
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Thread thread1 = new Thread(new ThreadStart(DoWork));
+            thread1.Start();
+        }
+    }
+}
---
+++ WpfApp7/WpfApp7/MainWindow.xaml.cs
@@ -0,0 +1,58 @@
+﻿using Notifications.Wpf;
+using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Windows;
+
+namespace WpfApp7
+{
+    public partial class MainWindow : Window
+    {
+        static int port = 8005; 
+        public MainWindow()
+        {
+            InitializeComponent();
+            IPEndPoint ipPoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), port);
+            Socket listenSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            try
+            {
+                listenSocket.Bind(ipPoint);
+                listenSocket.Listen(10);
+
+                MessageBox.Show("Сервер запущен. Ожидание подключений...");
+
+                while (true)
+                {
+                    Socket handler = listenSocket.Accept();
+                    StringBuilder builder = new StringBuilder();
+                    int bytes = 0; 
+                    byte[] data = new byte[256];
+                    do
+                    {
+                        bytes = handler.Receive(data);
+                        builder.Append(Encoding.Unicode.GetString(data, 0, bytes));
+                    }
+                    while (handler.Available > 0);
+
+                    var notificationManager = new NotificationManager();
+                    notificationManager.Show(new NotificationContent
+                    {
+                        Title = "Sample notification",
+                        Message = DateTime.Now.ToShortTimeString() + ": " + builder.ToString(),
+                        Type = NotificationType.Error
+                    });
+
+                    MessageBox.Show(DateTime.Now.ToShortTimeString() + ": " + builder.ToString());
+                    
+                    handler.Shutdown(SocketShutdown.Both);
+                    handler.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
